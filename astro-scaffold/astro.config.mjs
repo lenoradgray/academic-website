9aeb0645194ebb10f07e9284ac45a8fa11841f42
Lenora Gray <lgray34@jhu.edu>
2026-05-28 18:12:35 -0400

    new url

--- astro-scaffold/astro.config.mjs
+++ astro-scaffold/astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // CHANGE this to your deployed URL before building for production
-  site: 'https://843b5e18.academic-website-dsf.pages.dev',
+  site: 'https://academic-website-dsf.pages.dev',
   integrations: [
     tailwind({
       // We supply our own base styles in src/styles/global.css
