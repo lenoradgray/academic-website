1786febbefa0bb99d9d4c2986dc39e28f8168a99
Lenora Gray <lgray34@jhu.edu>
2026-05-28 18:18:55 -0400

    remove sitemap

--- astro-scaffold/astro.config.mjs
+++ astro-scaffold/astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // CHANGE this to your deployed URL before building for production
   site: 'https://academic-website-dsf.pages.dev',
   integrations: [
     tailwind({
-      // We supply our own base styles in src/styles/global.css
       applyBaseStyles: false,
     }),
-    sitemap(),
   ],
 });
